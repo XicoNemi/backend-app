4809da73dcf1e3b783d793610c2591e697d822f6
JazzoLopez <jazzielrodriguezlopez@gmail.com>
2024-11-03 00:57:01 -0600

    feat: Agregar servicio de correo electrónico y utilidades de autenticación

---
+++ src/utils/urlVerification.ts
@@ -0,0 +1,5 @@
+export const urlVerification = {
+    'local': 'http://localhost:11111/api/user/verify-email/',
+    'production': 'https://xiconemi.com/api/verify-email/',
+    'development': 'https://xiconemi.com/api/verify-email/'
+}
