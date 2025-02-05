fe2252449914dfe22d2f2f158b1f1a1dc9a73ce5
Luis Octavio <octaviodevtech@gmail.com>
2025-02-04 23:16:22 -0600

    fix: corect send Promise signIn

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -68,24 +68,31 @@ export const signUp = async (req: Request, res: Response) => {
   res.header('token', token).json(user).status(201);
 };
 
-export const signIn = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+export const signIn = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
 
-  // ? verify user
-  const user = await userModel.getUserByEmail(email);
-  if (!user) {
-    res.status(404).json({ message: 'User not found' }); // ! user not found
-    return;
+    // ? verify user
+    const user = await userModel.getUserByEmail(email);
+    if (!user) {
+      res.status(404).json({ message: 'User not found' }); // ! user not found
+      return;
+    }
+
+    // ? verify password
+    const isPasswordValid: boolean = await comparePassword(password, user.password);
+    if (!isPasswordValid) {
+      res.status(400).json({ message: 'Wrong password or email' }); // ! wrong password
+      return;
+    }
+
+    const token = await generateToken(user.id);
+    user.password = '';
+    res.status(200).header('auth-token', token).json(user);
+  } catch (error) {
+    console.error(error);
+    if (!res.headersSent) {
+      res.status(500).json({ message: 'Internal server error' });
+    }
   }
-
-  // ? verify password
-  const isPasswordValid: boolean = await comparePassword(password, user.password);
-  if (!isPasswordValid) {
-    res.status(400).json({ message: 'Wrong password or email' }); // ! wrong password
-    return;
-  }
-
-  const token = await generateToken(user.id);
-  user.password = '';
-  res.status(200).header('auth-token', token).json(user);
 };
