1d22b6de1b7acbe0eb22510d1cbea27de16ec75b
Luis Octavio <octaviodevtech@gmail.com>
2025-02-27 17:26:47 -0600

    fix: package.json templates

--- src/utils/urlVerification.ts
+++ src/utils/urlVerification.ts
@@ -1,10 +1,10 @@
 export const urlVerification = {
   local: 'http://localhost:11111/api/users/verify-email/',
-  production: 'https://backend-app-84du.onrender.com/api/users/verify-email/',
+  production: 'https://available-karlotta-ethdev11-59ebf81c.koyeb.app/api/users/verify-email/',
   development: 'https://backend-app-84du.onrender.com/api/users/verify-email/',
 };
 
 export const urlSwagger = {
   local: 'http://localhost:11111/api',
-  production: 'https://backend-app-84du.onrender.com/api',
+  production: 'https://available-karlotta-ethdev11-59ebf81c.koyeb.app/api',
 };
