cfafd7448b91d904bcc5f701a5ed3711e2bdf0f0
Luis Octavio <octaviodevtech@gmail.com>
2025-04-08 13:24:15 -0600

    add: rate limiting

--- src/config/limiter.ts
+++ src/config/limiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 m
-    max: 10,
+    max: 100,
     message: 'Demasiadas solicitudes, por favor intente más tarde.',
     standardHeaders: true,
     legacyHeaders: false
